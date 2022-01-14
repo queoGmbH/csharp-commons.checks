3043951c18308249de8a208ed6de458a128d10c3
Uwe Oehmichen <u.oehmichen@queo.onmicrosoft.com>
2022-01-14 21:28:10 +0100

    Fixes AB#8 multiple targets for cake build

---
+++ build/Build/Targets/BuildAndTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cake.Frosting;
+
+namespace Build
+{
+    public partial class BuildAndTest : FrostingTask<Context>
+    {
+        public override void Run(Context context)
+        {
+        }
+    }
+}
